d4c8577b479fd3c2ccade4f439db53f9c984cf15
Kangkook Jee <jikk@cs.columbia.edu>
2014-03-05 17:53:12 -0500

    main compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <stdint.h>
-#include "rbuf_nway.h"
+#include <sched.h>
+#include <unistd.h>
+#include "rbuf.h"
 
 uint32_t ring_buffer[RBUF_HIGH];
 uint32_t local_rbuf_idx = 0;
 
 /* Setting for 8 sub-buffers */
-volatile uint32_t nway_ctrl[(1 << NUM_CHUNK)] = {0, 0, 0, 0, 0, 0, 0, 0};  
+volatile uint32_t nway_ctrl[(1 << NUM_CHUNK)] = {0, 0, 0, 0};  
 
 /* variables local to primary */
 static uint8_t local_ctrl_idx = 0;
@@ -14,21 +16,21 @@ static uint32_t local_base = 0;
 static bool
 inline rbuf_overflow()
 {
-  return (*local_rbuf_idx - local_base) > NWAY_BOUND;
+  return (local_rbuf_idx - local_base) > NWAY_BOUND;
 }
 
 static void
-inline reset_rbuf_idx(UINT32 ent)
+inline reset_rbuf_idx()
 {
 
   //adding control command
-  rbuf[(*local_rbuf_idx)++] = RBUF_WRAP;
+  ring_buffer[local_rbuf_idx++] = RBUF_WRAP;
 
   // update ctrl_array -- access to shared variable
-  nway_ctrl[local_ctrl_idx] = *local_rbuf_idx - local_base;
+  nway_ctrl[local_ctrl_idx] = local_rbuf_idx - local_base;
 
-  //local update
-  local_base =  *local_rbuf_idx;
+  //update base
+  local_base =  local_rbuf_idx;
 
   // move local index
   local_ctrl_idx = ((local_ctrl_idx + 1) & ((1 << NUM_CHUNK) - 1));
@@ -40,42 +42,66 @@ inline reset_rbuf_idx(UINT32 ent)
 
   //resetting local idx
   if (local_ctrl_idx == 0) {
-    *local_rbuf_idx = 0;
+    local_rbuf_idx = 0;
     local_base = 0;
   }
-  rbuf[(*local_rbuf_idx)++] = ent;
 }
 
 static void
-inline reset_rbuf_idx_no_sec_chk(UINT32 ent)
+inline reset_rbuf_idx_no_worker()
 {
-
   //adding control command
-  rbuf[(*local_rbuf_idx)++] = RBUF_WRAP;
+  ring_buffer[local_rbuf_idx++] = RBUF_WRAP;
 
   // update ctrl_array -- access to shared variable
-  nway_ctrl[local_ctrl_idx] = *local_rbuf_idx - local_base;
+  nway_ctrl[local_ctrl_idx] = local_rbuf_idx - local_base;
 
   //local update
-  local_base =  *local_rbuf_idx;
+  local_base =  local_rbuf_idx;
 
   // move local index
   local_ctrl_idx = ((local_ctrl_idx + 1) & ((1 << NUM_CHUNK) - 1));
 
-  // polling for progress -- access to shared variable
-  while (nway_ctrl[local_ctrl_idx] != 0) {
-    usleep(50);
-  }
-
   //resetting local idx
   if (local_ctrl_idx == 0) {
-    *local_rbuf_idx = 0;
+    local_rbuf_idx = 0;
     local_base = 0;
   }
-  rbuf[(*local_rbuf_idx)++] = ent;
 }
-
 
 int main (int argc, char* argv[]){
-    ;
+
+//setting CPU affinity
+#ifdef __linux__
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+
+    CPU_SET(0, &mask);
+    // CPU_SET(2, &mask);
+    // CPU_SET(4, &mask);
+    // CPU_SET(6, &mask);
+    if (sched_setaffinity(0, sizeof(mask), &mask) < 0) {
+        perror("sched_setaffinity");
+    }
+#elif defined(__osx__)
+    thread_affinity_policy_data_t policy;
+	policy.affinity_tag = 0;
+	thread_policy_set(mach_thread_self(),
+			  THREAD_AFFINITY_POLICY,
+			  (thread_policy_t)&policy,
+			  THREAD_AFFINITY_POLICY_COUNT);
+#endif
+    while (1) {
+        for (int i=0; i < CHK_INTERVAL ; i++) {
+            ENQ(100);
+            INC(local_rbuf_idx, 1);
+        }
+        reset_rbuf_idx_no_worker();
+        if (cond) break;
+    }
+    //signal worker to get out.
+    ENQ(RBUF_EXIT);
+    INC(local_rbuf_idx, 1);
+    ENQ(RBUF_EXIT);
+    INC(local_rbuf_idx, 1);
 }
--- src/rbuf.h
+++ src/rbuf.h
@@ -1,25 +1,25 @@
 #ifndef __RBUF_H__
 #define __RBUF_H__
-
 /*
  * Ring buffer size
  */
 
 #define RBUF_FACTOR 4
 
-#define RBUF_FULL 0xFFFFFFFFU /* kernel ending address -- 3 2bit */
+/* kernel ending address for 32bit address space */
+#define RBUF_FULL 0xFFFFFFFFU
 
 /* RBUF size choices */
 
-#define RBUF_SIZE 0x7FFF  * RBUF_FACTOR    /* 32KB  * 4 * 4 bytes  */
-/* #define RBUF_SIZE 0xFFFF  * RBUF_FACTOR    /\* 64KB * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0x3FFF  * RBUF_FACTOR    /\* 16KB  * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0x7FFF  * RBUF_FACTOR    /\* 32KB  * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0xFFFF  * RBUF_FACTOR    /\* 64KB  * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0x1FFFF  * RBUF_FACTOR    /\* 128KB * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0x3FFFF  * RBUF_FACTOR    /\* 256KB  * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0x7FFFF  * RBUF_FACTOR    /\* 512KB  * 4 * 4 bytes  *\/ */
-/* #define RBUF_SIZE 0xFFFFF  * RBUF_FACTOR    /\* 1024KB  * 4 * 4 bytes  *\/ */
+// #define RBUF_SIZE 0x3FFF  * RBUF_FACTOR    /* 16KB * 4 * 4 bytes    */
+// #define RBUF_SIZE 0x7FFF  * RBUF_FACTOR    /* 32KB * 4 * 4 bytes    */
+// #define RBUF_SIZE 0xFFFF  * RBUF_FACTOR    /* 64KB * 4 * 4 bytes    */
+// #define RBUF_SIZE 0x1FFFF * RBUF_FACTOR    /* 128KB * 4 * 4 bytes   */
+// #define RBUF_SIZE 0x3FFFF * RBUF_FACTOR    /* 256KB * 4 * 4 bytes   */
+// #define RBUF_SIZE 0x7FFFF * RBUF_FACTOR    /* 512KB * 4 * 4 bytes   */
+// #define RBUF_SIZE 0xFFFFF * RBUF_FACTOR    /* 1024KB * 4 * 4 bytes  */
+
+#define RBUF_SIZE 0x7FFF  * RBUF_FACTOR    /* 32KB * 4 * 4 bytes    */
 
 #define RBUF_HIGH RBUF_SIZE * 2
 
@@ -29,5 +29,20 @@
 #define INIT_STATE   RBUF_FULL - 3
 #define FINI_STATE   RBUF_FULL - 4
 #define RBUF_EXIT    FINI_STATE
+
+/* N-way buffer setting */
+#define NUM_CHUNK 2
+#define NWAY_BOUND (RBUF_SIZE >> NUM_CHUNK)
+
+/* MACROs for ENQ and INC operation */
+#define ENQ(idx, ea) ring_buffer[(idx)] = ea
+
+/* LEA instruction used not to pollute eflag register */
+#define INC(idx, num)             \
+  __asm__ (                       \
+  "lea 1(%1), %0\n"               \
+  : "=r" (idx)                    \
+  : "r" (idx)                     \
+)
 
 #endif /* __RBUF_H__ */
